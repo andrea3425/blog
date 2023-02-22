f57925e1922addb64dacb0f06cb1bc8149c83dd1
Andrea Marinelli <andreamarinelli06@gmail.com>
2023-02-22 11:40:59 +0100

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path 
+from .views import BlogListView, BlogDetailView
+
+urlpatterns = [
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"), 
+    path("", BlogListView.as_view(), name="home"),
+]
+
+''' What is "pk"? It stands for Primary Key
+
+All blog post entries will start with post/. Next is the primary key 
+for our post entry which will be represented as an integer, <int:pk>. 
+
+What’s the primary key you’re probably asking? Django automatically adds 
+an auto-incrementing primary key86 to our database models. So while we only 
+declared the fields title, author, and body on our Post model, under-the-hood 
+Django also added another field called id, which is our primary key. 
+
+We can access it as either id or pk. '''
